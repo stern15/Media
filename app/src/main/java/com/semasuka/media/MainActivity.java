886b2a8439a276277c4424b880484587203578e2
stern15 <ssemasuka@gmail.com>
2016-03-25 13:48:05 +0300

    rotation upside down

--- app/src/main/java/com/semasuka/media/MainActivity.java
+++ app/src/main/java/com/semasuka/media/MainActivity.java
@@ -4,25 +4,29 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void fade(View view) {
-        ImageView peter = (ImageView) findViewById(R.id.peter);
+
       /* ImageView stewie=(ImageView) findViewById(R.id.stewie);
 
       if(peter.getAlpha() == 1) {
         */
+        ImageView peter = (ImageView) findViewById(R.id.peter);
 
-        peter.animate().translationXBy(1000f).setDuration(2000);
+        Toast.makeText(getApplicationContext(),"clicked",Toast.LENGTH_SHORT).show();
+
+        if(peter.getRotation()==0)
+            peter.animate().rotation(180f).setDuration(2000);
+        else
+            peter.animate().rotation(0f).setDuration(2000);
+//        view.animate().rotation(360f).setDuration(2000);
      /*  stewie.animate().alpha(1f).setDuration(2000);
 
 
-        */
-        peter.setTranslationX(1000f);
-
-/*
-    }
+  }
        else {
            peter.animate().alpha(1f).setDuration(2000);
             stewie.animate().alpha(0f).setDuration(2000);
@@ -38,14 +42,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+/*
         ImageView peter=(ImageView) findViewById(R.id.peter);
         peter.setTranslationX(-1000f);
 
-      /*  ImageView peter = (ImageView) findViewById(R.id.peter);
+        ImageView peter = (ImageView) findViewById(R.id.peter);
         peter.setTranslationX(-1500);*/
 
+        /*Button peterbtn = (Button)findViewById(R.id.button);
+        final ImageView peter = (ImageView) findViewById(R.id.peter);
+        peterbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView peter = (ImageView) findViewById(R.id.peter);
+
+                Toast.makeText(getApplicationContext(),"clicked",Toast.LENGTH_SHORT).show();
+
+                if(peter.getRotation()==0)
+                peter.animate().rotation(360f).setDuration(2000);
+                else
+                    peter.animate().rotation(0f).setDuration(2000);
+            }
+        });
+
+    }*/
+
     }
-
-
 }
