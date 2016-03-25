85a244473d5a7401949b753da1050b72d0a31d25
stern15 <ssemasuka@gmail.com>
2016-03-25 12:36:25 +0300

    transition left and right

--- app/src/main/java/com/semasuka/media/MainActivity.java
+++ app/src/main/java/com/semasuka/media/MainActivity.java
@@ -9,23 +9,26 @@ public class MainActivity extends AppCompatActivity {
 
     public void fade(View view) {
         ImageView peter = (ImageView) findViewById(R.id.peter);
-        ImageView stewie=(ImageView) findViewById(R.id.stewie);
+      /* ImageView stewie=(ImageView) findViewById(R.id.stewie);
 
-       if(peter.getAlpha() == 1) {
+      if(peter.getAlpha() == 1) {
+        */
+
+        peter.animate().translationXBy(1000f).setDuration(2000);
+     /*  stewie.animate().alpha(1f).setDuration(2000);
 
 
-        peter.animate().alpha(0f).setDuration(2000);
-        stewie.animate().alpha(1f).setDuration(2000);
+        */
+        peter.setTranslationX(1000f);
 
-
-
+/*
     }
        else {
-            peter.animate().alpha(1f).setDuration(2000);
+           peter.animate().alpha(1f).setDuration(2000);
             stewie.animate().alpha(0f).setDuration(2000);
 
 
-        }
+       }*/
     }
 
 
@@ -36,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView peter=(ImageView) findViewById(R.id.peter);
+        peter.setTranslationX(-1000f);
 
       /*  ImageView peter = (ImageView) findViewById(R.id.peter);
         peter.setTranslationX(-1500);*/
