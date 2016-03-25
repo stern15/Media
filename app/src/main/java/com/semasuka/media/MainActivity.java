d22ad7834b07a04bd09e6b0fe1bb71e2032af30b
stern15 <ssemasuka@gmail.com>
2016-03-25 14:07:20 +0300

    all combinated transition

--- app/src/main/java/com/semasuka/media/MainActivity.java
+++ app/src/main/java/com/semasuka/media/MainActivity.java
@@ -4,35 +4,34 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void fade(View view) {
 
-      /* ImageView stewie=(ImageView) findViewById(R.id.stewie);
 
-      if(peter.getAlpha() == 1) {
-        */
         ImageView peter = (ImageView) findViewById(R.id.peter);
 
-        Toast.makeText(getApplicationContext(),"clicked",Toast.LENGTH_SHORT).show();
+        if (peter.getScaleX()==1){
 
-        if(peter.getRotation()==0)
-            peter.animate().rotation(180f).setDuration(2000);
-        else
-            peter.animate().rotation(0f).setDuration(2000);
-//        view.animate().rotation(360f).setDuration(2000);
-     /*  stewie.animate().alpha(1f).setDuration(2000);
+        peter.animate()
+                .scaleX(0.5f)
+                .scaleY(0.5f)
+                .alpha(0.5f)
+                .rotation(180f)
+                .translationX(200f)
+                .setDuration(2000);}
 
+        else {
 
-  }
-       else {
-           peter.animate().alpha(1f).setDuration(2000);
-            stewie.animate().alpha(0f).setDuration(2000);
+            peter.animate()
+                    .scaleX(1f)
+                    .scaleY(1f)
+                    .alpha(1f)
+                    .rotation(0f)
+                    .translationX(0f)
+                    .setDuration(2000);}
 
-
-       }*/
     }
 
 
@@ -42,30 +41,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
-        ImageView peter=(ImageView) findViewById(R.id.peter);
-        peter.setTranslationX(-1000f);
 
-        ImageView peter = (ImageView) findViewById(R.id.peter);
-        peter.setTranslationX(-1500);*/
-
-        /*Button peterbtn = (Button)findViewById(R.id.button);
-        final ImageView peter = (ImageView) findViewById(R.id.peter);
-        peterbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ImageView peter = (ImageView) findViewById(R.id.peter);
-
-                Toast.makeText(getApplicationContext(),"clicked",Toast.LENGTH_SHORT).show();
-
-                if(peter.getRotation()==0)
-                peter.animate().rotation(360f).setDuration(2000);
-                else
-                    peter.animate().rotation(0f).setDuration(2000);
-            }
-        });
-
-    }*/
 
     }
 }
